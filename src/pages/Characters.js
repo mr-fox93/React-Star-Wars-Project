f1e219e960c24cb11da5953b8a2b40e0b9a0690c
mr-fox93 <lisieckikamil93@gmail.com>
2023-04-20 22:17:48 +0200

    add .semantic-ui table

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,20 +1,13 @@
-import { useQuery } from "react-query";
-
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import "./Characters.css";
-
 import React, { useState } from "react";
+import { Icon, Label, Menu, Table } from "semantic-ui-react";
+import { useQuery } from "react-query";
+import "./Characters.css";
 
 const Characters = () => {
   const [page, setPage] = useState(1);
+  const [value, setValue] = useState("");
 
-  const { data, isLoading, error } = useQuery("people", () =>
+  const { data, isLoading, error } = useQuery(["people", page], () =>
     fetch(`https://swapi.dev/api/people/?page=${page}`).then((res) =>
       res.json()
     )
@@ -23,30 +16,66 @@ const Characters = () => {
   if (isLoading) return <div>Loading ...</div>;
   if (error) return <div>Error !</div>;
 
+  const filtredItems = data.results.filter((item) => {
+    return item.name.toLowerCase().includes(value.toLowerCase());
+  });
+
   return (
     <div className="tabel">
-      <TableContainer className="container" component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Species</TableCell>
-              <TableCell>Status</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.results.map((character) => (
-              <TableRow key={character.id}>
-                <TableCell component="th" scope="row">
-                  {character.name}
-                </TableCell>
-                <TableCell>{character.gender}</TableCell>
-                <TableCell>{}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <div className="header">
+        <input
+          className="input"
+          type="text"
+          value={value}
+          placeholder="search"
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Species</Table.HeaderCell>
+            <Table.HeaderCell>Status</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {filtredItems.map((character) => (
+            <Table.Row key={character.id}>
+              <Table.Cell>{character.name}</Table.Cell>
+              <Table.Cell>{character.gender}</Table.Cell>
+              <Table.Cell>{}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan="3">
+              <Menu floated="right" pagination>
+                <Menu.Item
+                  as="a"
+                  icon
+                  disabled={page === 1}
+                  onClick={() => setPage((prev) => prev - 1)}
+                >
+                  <Icon name="chevron left" />
+                </Menu.Item>
+                <Menu.Item as="a" active>
+                  {page}
+                </Menu.Item>
+                <Menu.Item
+                  as="a"
+                  icon
+                  disabled={data.next === null}
+                  onClick={() => setPage((prev) => prev + 1)}
+                >
+                  <Icon name="chevron right" />
+                </Menu.Item>
+              </Menu>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
     </div>
   );
 };
